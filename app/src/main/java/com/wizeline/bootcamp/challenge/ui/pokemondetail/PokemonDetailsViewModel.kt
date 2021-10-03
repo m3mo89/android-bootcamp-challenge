16eb5f72434dd6bc2938be59f8e7cfefeea24dc5
Guillermo Tinoco <m3mo89@gmail.com>
2021-10-02 19:12:05 -0500

    Implement a function that communicates with the PokemonDetailsRepo to get the needed info

--- app/src/main/java/com/wizeline/bootcamp/challenge/ui/pokemondetail/PokemonDetailsViewModel.kt
+++ app/src/main/java/com/wizeline/bootcamp/challenge/ui/pokemondetail/PokemonDetailsViewModel.kt
@@ -1,8 +1,10 @@
 package com.wizeline.bootcamp.challenge.ui.pokemondetail
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.wizeline.bootcamp.challenge.data.mock.PokemonDetails
 import com.wizeline.bootcamp.challenge.data.repo.PokemonDetailsRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +12,22 @@ class PokemonDetailsViewModel @Inject constructor(
     private val pokemonDetailsRepo: PokemonDetailsRepo
 ) : ViewModel() {
 
-    // TODO DESIGN PATTERNS: Implement a function that communicates with the PokemonDetailsRepo to get the needed info.
+    // Implement a function that communicates with the PokemonDetailsRepo to get the needed info.
+    private val _id = MutableLiveData<String>()
+
+    private val _pokemon = _id.switchMap { id ->
+        liveData(Dispatchers.IO)
+        {
+            val response = pokemonDetailsRepo.getPokemonDetails(id)
+
+            val pokemonDetail = response.body()
+            emit(pokemonDetail!!)
+        }
+    }
+
+    val pokemon : LiveData<PokemonDetails> = _pokemon
+
+    fun init(id: String){
+        _id.value = id
+    }
 }
